550a729dc714ea05c57a37d966b7372dcf687d67
Dengxian ji <99891555+jadenjjjj@users.noreply.github.com>
2023-02-06 17:00:57 +0000

    ite3.1

--- routes/celebrities-routes.js
+++ routes/celebrities-routes.js
@@ -1,1 +1,8 @@
 const router = require('express').Router()
+
+const Celebrity = require('./models/Celebrity.model.js')
+
+router.get('/new', (req, res, next) => {
+    res.render('celebrities/new-celebrity');
+})
+
